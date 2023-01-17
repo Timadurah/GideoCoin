0425262f30d613866b840057ce6c496380f4373b
kilogrammie <117412844+kilogrammie@users.noreply.github.com>
2023-01-17 14:09:32 -0800

    Add files via upload
    Dashboard template

---
+++ dashboard/dashboard.js
@@ -0,0 +1,25 @@
+
+// carosel javascript
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+});
